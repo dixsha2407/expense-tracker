2d3008639a347c800ac503e8d21bfbc824496f9a
dixsha2407 <patildiksha99@gmail.com>
2020-07-11 15:57:25 +0530

    second commit

--- expense.py
+++ expense.py
@@ -1,19 +1,59 @@
-income=int(input())
-dict1={}
-while input("Enter q to quit,c to continue ") != 'q':
-    key=input("enter item")
-    while True: 
-        value1=int(input("enter price "))
-        if not type(value1) is int:
-            print("enter number")
+print("Welcome to your daily expense tracker")
+print("Enter Login details")
+while True:
+    userid=input("enter userid")
+    if userid!='diksha':
+        continue
+    password=input("your password?")
+    if password=='patil':
+        break
+
+income=int(input("Enter your monthly income"))
+expense={}
+while True:  
+    item=input("enter item purchased")
+    while True:
+        try:
+            price=int(input("enter price "))
+        except ValueError:
+            print("invalid input")
         else:
             break
-    dict1.update({key:value1})
-print(dict1)
-spent= (sum(dict1.values()))
-money= income-spent
-if money<0:
-    print("you extra spent ",abs(money))
+    expense.update({item:price})
+    response=input("enter q to quit,any key to continue")
+    if response=='q'or response=='Q':
+        break
+print(expense)
+totalexp=sum(expense.values)
+savings=income-totalexp
+
+def luxury():
+    print("you saved"+str(savings))
+    print("Enter your luxury list")
+    luxury={}
+    while True:  
+        lxitem=input("enter luxury item ")
+        while True:
+            try:
+                lxprice=int(input("enter item price "))
+            except ValueError:
+                print("invalid input")
+            else:
+                break
+        luxury.update({lxitem:lxprice})
+        res=input("enter q to quit,any key to continue")
+        if res=='q'or res=='Q':
+            break
+    
+        
+
+
+
+if savings<0:
+    print("you extra spent"+str(abs(savings)))
 else:
-    print("you saved ",money)
+    luxury()
+
+
+
 
